77801eb4bfbaa71f228fd790a877eff731d80a68
izereuwonkunda <izereuwonkundakelia@gmail.com>
2021-12-16 17:16:37 +0200

    final push

--- src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
+++ src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
@@ -1,5 +1,74 @@
 package rw.ac.rca.termOneExam.service;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.any;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CityServiceTest {
+    @Mock
+    private ICityRepository cityRepositoryMock;
+
+    @InjectMocks
+    private CityService cityService;
+    @Test
+    public void getAll(){
+        when(cityRepositoryMock.findAll())
+                .thenReturn(Arrays.asList(new City(101,"Kigali",24,75.2),
+                        new City(102,"Musanze",18,64.4),new City(103,"Rubavu",20,68),new City(104,"Nyagatare",28,82.4)));
+
+        assertEquals(4,cityService.getAll().size());
+        assertEquals("Kigali",cityService.getAll().get(0).getName());
+    }
+    @Test
+    public void getById_Success(){
+        City city=new City(102,"Musanze",18,64.4);
+        when(cityRepositoryMock.findById(city.getId())).thenReturn(Optional.of(city));
+        assertEquals(102,cityService.getById(city.getId()).get().getId());
+        assertEquals(64.4,cityService.getById(city.getId()).get().getFahrenheit());
+    }
+//    @Test
+//    public void getById_notfound() {
+//        when(cityRepositoryMock.findById(anyLong())).thenReturn(Optional.empty());
+//
+//        assertTrue(null,cityService.getById(108).isPresent());
+//    }
+    @Test
+    public void save_Success(){
+        when(cityRepositoryMock.save(ArgumentMatchers.any(City.class))).thenReturn(new City("Gakenke", 23));
+
+        CreateCityDTO dto = new CreateCityDTO();
+        dto.setName("Gakenke");
+        dto.setWeather(23);
+
+        assertEquals("Gakenke", cityService.save(dto).getName());
+        assertEquals(23, cityService.save(dto).getWeather());
+
+    }
+    @Test
+    public void existsByName_test() {
+        when(cityRepositoryMock.existsByName(anyString())).thenReturn(true);
+
+        assertTrue(cityService.existsByName("Musanze"));
+    }
+
 
 }
--- src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -1,5 +1,114 @@
 package rw.ac.rca.termOneExam.utils;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+import rw.ac.rca.termOneExam.service.CityService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class CityUtilTest {
+    @Autowired
+    private ICityRepository cityRepository;
+
+    @Test
+    public void NoWeather_greater40() {
+
+
+        ArrayList<City> list= (ArrayList<City>) cityRepository.findAll();
+		int i = 0;
+		while (i < list.size()) {
+
+		assertTrue(list.get(i).getWeather()<40);
+        i++;
+		}
+
+    }
+    @Test
+    public void NoWeather_less10() {
+
+
+        ArrayList<City> list= (ArrayList<City>) cityRepository.findAll();
+        int i = 0;
+        while (i < list.size()) {
+
+            assertTrue(list.get(i).getWeather()>10);
+            i++;
+        }
+
+    }
+    @Test
+    public void Cities_Kigali_Musanze() {
+         ArrayList<String> cities=new ArrayList<>();
+        ArrayList<City> list= (ArrayList<City>) cityRepository.findAll();
+        int i = 0;
+        while (i < list.size()) {
+
+           cities.add(list.get(i).getName());
+            i++;
+        }
+        assertTrue(cities.contains("Kigali"));
+         assertTrue(cities.contains("Musanze"));
+    }
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class TestingSpy {
+
+        @Spy
+        private ICityRepository cityRepositoryBySpy;
+
+        @InjectMocks
+        private CityService cityServiceMock;
+
+        @Test
+        public void testSpying() {
+            when(cityRepositoryBySpy.findAll()).thenReturn(Arrays.asList(new City("Burera", 18), new City("Huye", 26),new City("Gakenke", 22)));
+
+            List<City> cities = cityServiceMock.getAll();
+
+            assertEquals(3, cities.size());
+        }
+
+    }
+
+
+    @RunWith(MockitoJUnitRunner.class)
+    public static class TestingMock {
+
+        @Mock
+        private ICityRepository cityRepositoryByMock;
+
+        @InjectMocks
+        private CityService cityServiceMock;
+
+        @Test
+        public void testMocking() {
+            when(cityRepositoryByMock.findAll()).thenReturn(Arrays.asList(new City("Burera", 18), new City("Huye", 26),new City("Gakenke", 22)));
+
+            List<City> cities = cityServiceMock.getAll();
+
+            assertEquals(3, cities.size());
+        }
+
+    }
 
 }
